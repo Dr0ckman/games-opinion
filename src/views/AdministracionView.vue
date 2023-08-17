d2f95268a56d1e496532c6798442f7ae40d3e3c7
Dr0ckman <cmn.kkckdbb@gmail.com>
2023-08-17 08:28:14 -0400

    Reorganized files

--- src/views/AdministracionView.vue
+++ src/views/AdministracionView.vue
@@ -3,14 +3,14 @@
         <Sidebar></Sidebar>
         <div class="container-fluid">
             <h1>Resumen de tu cuenta</h1>
-            <p>Le diste me gusta al juego {{ this.$route.query.game }}</p>
+            <p>Le diste me gusta al juego <strong>{{ this.$route.query.game }}</strong></p>
             <p>Acá va el componente para agregar monedas</p>
         </div>
     </div>
 </template>
 
 <script>
-import Sidebar from '@/components/Sidebar.vue'
+import Sidebar from '@/components/administracion/Sidebar.vue'
 import Login from '@/components/login/Login.vue'
 
 export default {
